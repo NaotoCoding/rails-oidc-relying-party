105896ddc2e302ef43acedb74f932afc53574ee8
NaotoCoding <naoto.zaniah@gmail.com>
2024-07-06 22:47:33 +0900

    usersテーブルのカラムを変更し、migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_06_133943) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "uid", null: false
+    t.string "provider", null: false
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,19 +2,13 @@
 #
 # Table name: users
 #
-#  id                     :bigint           not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  remember_created_at    :datetime
-#  reset_password_sent_at :datetime
-#  reset_password_token   :string
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
+#  id       :bigint           not null, primary key
+#  provider :string           not null
+#  uid      :string           not null
 #
 # Indexes
 #
-#  index_users_on_email                 (email) UNIQUE
-#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_uid_and_provider  (uid,provider) UNIQUE
 #
 FactoryBot.define do
   factory :user do
