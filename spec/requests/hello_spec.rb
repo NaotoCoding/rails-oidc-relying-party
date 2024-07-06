3de7b998e2c25e2e5af88cd0bf8a57c2909295f0
NaotoCoding <naoto.zaniah@gmail.com>
2024-07-06 22:57:52 +0900

    hello#indexの画面を表示

--- spec/requests/hello_spec.rb
+++ spec/requests/hello_spec.rb
@@ -1,7 +1,17 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Hellos", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /" do
+    it "200を返す" do
+      get root_path
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe "GET /hello" do
+    it "200を返す" do
+      get hello_index_path
+      expect(response).to have_http_status 200
+    end
   end
 end
