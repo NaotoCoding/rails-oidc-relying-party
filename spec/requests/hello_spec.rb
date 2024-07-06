35e37b411ec26a7b6d44f6e2c862699c0e261f2d
NaotoCoding <naoto.zaniah@gmail.com>
2024-07-06 23:02:56 +0900

    applicaiton_controller下のアクションを実行する前にログインを必須とした

--- spec/requests/hello_spec.rb
+++ spec/requests/hello_spec.rb
@@ -1,8 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "Hellos", type: :request do
+  describe "ログインしていない場合" do
+    it "ログイン画面にリダイレクトされる" do
+      get root_path
+      expect(response).to have_http_status 302
+    end
+  end
+
   describe "GET /" do
     it "200を返す" do
+      sign_in create(:user)
       get root_path
       expect(response).to have_http_status 200
     end
@@ -10,6 +18,7 @@ RSpec.describe "Hellos", type: :request do
 
   describe "GET /hello" do
     it "200を返す" do
+      sign_in create(:user)
       get hello_index_path
       expect(response).to have_http_status 200
     end
